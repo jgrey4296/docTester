bdeccbfc7d1034e6998a01096293f3b8082ff5de
John Grey <jgrey@ucsc.edu>
2017-03-11 18:29:40 +0000

    added fail test separation

--- doctester/DocTestRunner.py
+++ doctester/DocTestRunner.py
@@ -6,16 +6,19 @@ class DocTestRunner:
     """ The main test runner class. Subclass this to add tests """
     #Usefule constants:
     TEST_NAME = 'test_'
+    FAIL = 'test_fail_'
     TICK = '✓'
     CROSS = 'X' 
     TAB =  "	"
-
+    
     def __call__(self):
         """ The main call to run tests """
         passed = 0
         failed = 0
-        tests = [x for x in dir(self) if DocTestRunner.TEST_NAME in x]
-        total = len(tests)
+        tests = [x for x in dir(self) if DocTestRunner.TEST_NAME in x and DocTestRunner.FAIL not in x]
+        fail_tests = [x for x in dir(self) if DocTestRunner.FAIL in x]
+        total = len(tests)+len(fail_tests)
+        logging.info("Running Tests:")
         for test in tests:
             try:
                 result = getattr(self,test)()
@@ -23,11 +26,11 @@ class DocTestRunner:
                     raise DocException('Test returned False')
             except DocException as e:
                 logging.warning("{} {} : {} {} {} {} {}".format(DocTestRunner.TAB,
-                                                        DocTestRunner.CROSS,
-                                                        test,
-                                                        '\n',
-                                                        DocTestRunner.TAB, DocTestRunner.TAB,
-                                                        repr(e)))
+                                                                DocTestRunner.CROSS,
+                                                                test,
+                                                                '\n',
+                                                                DocTestRunner.TAB, DocTestRunner.TAB,
+                                                                repr(e)))
                 failed += 1
             else:
                 logging.info("{} {} : {}".format(DocTestRunner.TAB,
@@ -35,4 +38,17 @@ class DocTestRunner:
                                                  test))
                 passed += 1
 
+        logging.info('Running Fail Tests:')
+        for test in fail_tests:
+            try:
+                result = getattr(self,test)()
+                if result is not None and result != False:
+                    logging.warning('{} : Did not return false'.format(test))
+                    failed += 1
+            except DocException as e:
+                passed += 1
+                logging.info("{} {} : {}".format(DocTestRunner.TAB,DocTestRunner.TICK, test))
+                    
+                
         logging.info("\n\nFinished. {}/{} Passed. {}/{} Failed.".format(passed,total,failed,total)) 
+
