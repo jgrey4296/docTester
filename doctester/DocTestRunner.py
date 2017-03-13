22b584cc32aa222f7c2251aafaa372c56285dd88
John Grey <jgrey@ucsc.edu>
2017-03-13 10:30:26 +0000

    formatting to pass linting

--- doctester/DocTestRunner.py
+++ doctester/DocTestRunner.py
@@ -1,6 +1,10 @@
+"""
+Module Defines the Base Class for Automating Document Tests
+"""
 import logging as root_logger
-logging = root_logger.getLogger(__name__)
 from doctester.DocException import DocException
+logging = root_logger.getLogger(__name__)
+
 
 class DocTestRunner:
     """ The main test runner class. Subclass this to add tests """
@@ -8,29 +12,31 @@ class DocTestRunner:
     TEST_NAME = 'test_'
     FAIL = 'test_fail_'
     TICK = '✓'
-    CROSS = 'X' 
-    TAB =  "	"
-    
+    CROSS = 'X'
+    TAB = "	"
+
     def __call__(self):
         """ The main call to run tests """
         passed = 0
         failed = 0
-        tests = [x for x in dir(self) if DocTestRunner.TEST_NAME in x and DocTestRunner.FAIL not in x]
+        tests = [x for x in dir(self) if DocTestRunner.TEST_NAME in x and \
+                 DocTestRunner.FAIL not in x]
         fail_tests = [x for x in dir(self) if DocTestRunner.FAIL in x]
         total = len(tests)+len(fail_tests)
         logging.info("Running Tests:")
         for test in tests:
             try:
-                result = getattr(self,test)()
-                if result is not None and result == False:
+                result = getattr(self, test)()
+                if result is not None and result is False:
                     raise DocException('Test returned False')
-            except DocException as e:
+            except DocException as err:
                 logging.warning("{} {} : {} {} {} {} {}".format(DocTestRunner.TAB,
                                                                 DocTestRunner.CROSS,
                                                                 test,
                                                                 '\n',
-                                                                DocTestRunner.TAB, DocTestRunner.TAB,
-                                                                repr(e)))
+                                                                DocTestRunner.TAB,
+                                                                DocTestRunner.TAB,
+                                                                repr(err)))
                 failed += 1
             else:
                 logging.info("{} {} : {}".format(DocTestRunner.TAB,
@@ -41,15 +47,17 @@ class DocTestRunner:
         logging.info('Running Fail Tests:')
         for test in fail_tests:
             try:
-                result = getattr(self,test)()
+                result = getattr(self, test)()
                 if result is None or result != False:
                     logging.info('{} {} : {}'.format(DocTestRunner.TAB,
-                                                                          DocTestRunner.CROSS,
-                                                                          test))
+                                                     DocTestRunner.CROSS,
+                                                     test))
                     failed += 1
-            except DocException as e:
+            except DocException as err:
                 passed += 1
-                logging.info("{} {} : {}".format(DocTestRunner.TAB,DocTestRunner.TICK, test))
-                    
-                
-        logging.info("\n\nFinished. {}/{} Passed. {}/{} Failed.".format(passed,total,failed,total))
+                logging.info("{} {} : {}".format(DocTestRunner.TAB,
+                                                 DocTestRunner.TICK,
+                                                 test))
+
+        log_str = "\n\nFinished. {}/{} Passed. {}/{} Failed."
+        logging.info(log_str.format(passed, total, failed, total))
