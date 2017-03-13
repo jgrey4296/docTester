b3b06ecc43f6137fdfc169d2b11e71316c0abc9c
John Grey <jgrey@ucsc.edu>
2017-03-13 01:18:47 +0000

    fixed textparser -> section construction

--- doctester/TextParser.py
+++ doctester/TextParser.py
@@ -41,32 +41,54 @@ ROOT = pp.OneOrMore(heading | paragraph)
 
 def parseText(text):
     """ Parse text, creating headings, paragraphs, tags, citations """
-    data = []
     separated = lineSep.split(text)
+    rootSection = None
     currentSection = None
     while len(separated) > 0:
         current = separated.pop(0)
-        results = ROOT.parseSring(current)
-        if isinstance(results[0],HEADER):
-            currentSection = {'title':result, paragraphs: [], tags: []}
-            data.append(currentSection)
+        results = ROOT.parseString(current)
+        #parsed a header, create a section
+        if len(results) == 0:
+            results = "Nothing, just add the paragraph"
+            
+        if isinstance(results[0],HEADER) and rootSection is None:
+            #a new, root section
+            currentSection = Section(results[0].title,results[0].level)
+            rootSection = currentSection
             for res in results[1:]:
                 if isinstance(res,TAG):
-                    currentSection['tags'].append(res)
+                    currentSection.add_tag(res.text)
+
+        elif isinstance(results[0],HEADER):
+            #a subsection
+            if results[0].level > currentSection.level:
+                currentSection = currentSection.add_subsection(results[0].title,
+                                                               results[0].level)
+            elif results[0].level <= currentSection.level:
+                #a subsection of an ancestor 
+                while results[0].level <= currentSection.level:
+                    currentSection = currentSection.get_parent()
+                    
+                currentSection = currentSection.add_subsection(results[0].title,
+                                                               results[0].level)
+            for res in results[1:]:
+                if isinstance(res,TAG):
+                    currentSection.add_tag(res.text)
+
         else:
-            currentParagraph = {'text': current, tags: [], citations: [] }
-            currentSection['paragraphs'].append(currentParagraph)
+            #Paragraphs of a section
+            currentParagraph = currentSection.add_paragraph(current)
             for res in results:
                 if isinstance(res,TAG):
-                    currentParagraph['tags'].append(res)
+                    currentParagraph['tags'].add(res.text)
                 elif isinstance(res,CITATION):
-                    currentParagraph['citations'].append(res)
+                    currentParagraph['citations'].add(res.text)
                         
-    return data
+    return rootSection
         
 #actions:
 heading_stars.setParseAction(lambda toks: len(toks))
-heading.setParseAction(lambda toks: HEADER(toks[0],toks[1]))
+heading.setParseAction(lambda toks: HEADER(toks[0],toks[1].strip()))
 
-tag.setParseAction(lambda toks: TAG(toks))
-citation.setParseAction(lambda toks: CITATION(toks))
+tag.setParseAction(lambda toks: TAG(" ".join(toks[:])))
+citation.setParseAction(lambda toks: CITATION(" ".join(toks[:])))
