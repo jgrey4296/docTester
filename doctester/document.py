0056eabe69bf7433ecc9a74021ce93d04764e346
John Grey <jgrey@ucsc.edu>
2017-03-13 10:48:15 +0000

    updating to linter spec

--- doctester/document.py
+++ doctester/document.py
@@ -1,8 +1,10 @@
+"""
+	Defines the Document class, top level collection of texts
+"""
 import logging as root_logger
-from os.path import join,isfile,exists,isdir, splitext
+from os.path import join, isdir, splitext
 from os import listdir
 from doctester.DocException import DocException
-from doctester.Section import Section
 from doctester.Should import Should
 from doctester.TextParser import parseText
 logging = root_logger.getLogger(__name__)
@@ -12,9 +14,11 @@ class Document:
     and allows access to 'should' testing """
     FILETYPE = '.org'
 
-    
-    def __init__(self,directory):
+
+    def __init__(self, directory):
         """ Given a directory, load in all files of FILETYPE, and create indiv chapters for them """
+        if not isdir(directory):
+            raise Exception("Bad Directory Specification: {}".format(directory))
         read_files = listdir(directory)
         org_files = [x for x in read_files if splitext(x)[1] == Document.FILETYPE]
         self.directory = directory
@@ -23,48 +27,50 @@ class Document:
         #Actually read in all found files
         self.read_files()
 
-    def __getattr__(self,value):
+    def __getattr__(self, value):
         #allows doc.should, instead of doc.should()
         #while retaining doc.read_files()
         if value == 'should':
             return Should(self)
         else:
             raise AttributeError('{} Not Suitable for Document'.format(value))
-        
+
     def read_files(self):
-        for x in self.files:
-            fullpath = join(self.directory,x)
-            title = splitext(x)[0]
-            with open(fullpath,'r') as f:
+        for file in self.files:
+            fullpath = join(self.directory, file)
+            title = splitext(file)[0]
+            with open(fullpath, 'r') as f:
                 text = f.read()
             #chapters are the same ds as sections
             self.chapters[title.lower().strip()] = parseText(text)
 
-    def chapter(self,name):
+    def chapter(self, name):
         #Get a chapter from the document, use the same error as 'should'ing if it fails
         fname = name.lower().strip()
         if fname in self.chapters:
             return self.chapters[fname]
         else:
-            raise DocException("No Chapter Found",missing=name)
+            raise DocException("No Chapter Found", missing=name)
 
     def get_word_count(self):
         return sum([x.get_word_count() for x in self.chapters.values()])
 
     def get_sentence_count(self):
         return sum([x.get_sentence_count() for x in self.chapters.values()])
-    
+
     def get_paragraph_count(self):
         return sum([x.get_paragraph_count() for x in self.chapters.values()])
 
     def get_citations(self):
+        """ Get the Union of all citation sets of all sub chapters/sections """
         base_set = set()
         citation_sets = [x.get_citations() for x in self.chapters.values()]
         for chapter in citation_sets:
             base_set = base_set.union(chapter)
         return base_set
 
-    def mentions(self,reference):
+    def mentions(self, reference):
+        """ Test for a literal string in the documents chapters/sections """
         for chapter in self.chapters.values():
             if chapter.mentions(reference):
                 return True
